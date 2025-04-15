921b5068176f16a8280f3821c38d13b6d9ac17b6
bdwsq1996 <48882328+bdwsq1996@users.noreply.github.com>
2025-04-15 15:10:47 +0800

    Add files via upload

--- train.sh
+++ train.sh
@@ -20,7 +20,7 @@ fi
 #train
 
 CUDA_VISIBLE_DEVICES=4,5,6,7 python -u train.py --mode='train' --epochs=16 --numdepth=384 --trainviews=5 --testviews=5 --logdir $LOG_DIR --dataset=dtu_yao --batch_size=16 --trainpath=$MVS_TRAINING \
-                --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --GRUiters 3,3,3 --ndepths 48,8,8 --CostNum=3 --initloss=initloss --lr=0.001 --dispmaxfirst=last  --maskupmode=eachstage --interval_scale=0.53 --lossrate=0.9 | tee -i $dirAndName
+                --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --GRUiters 3,3,3 --ndepths 48,8,8 --CostNum=3 --initloss=initloss --lr=0.001 --dispmaxfirst=last  --maskupmode=eachstage --interval_scale=0.53 --lossrate=1.0 | tee -i $dirAndName
 #finetune
 #CUDA_VISIBLE_DEVICES=4,5,6,7 python -u train.py --mode='finetune' --epochs=10 --numdepth=384 --trainviews=7 --testviews=7 --logdir $LOG_DIR --loadckpt $LOG_DIR_CKPT --dataset=blend --batch_size=8 --trainpath=$BLEND_TRAINING \
-#                --trainlist lists/dtu/train.txt --testlist lists/dtu/test.txt --ndepths=96 --CostNum=3 --initloss=initloss --lr=0.0004 --dispmaxfirst=last  --maskupmode=eachstage --interval_scale=0.53  --lossrate=0.9 | tee -i $dirAndName
+#                --trainlist lists/bld/training_list.txt --testlist lists/bld/validation_list.txt --ndepths=96 --CostNum=3 --initloss=initloss --lr=0.0004 --dispmaxfirst=last  --maskupmode=eachstage --interval_scale=0.53  --lossrate=1.0 | tee -i $dirAndName
